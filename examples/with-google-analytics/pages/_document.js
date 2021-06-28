50780eb12244a89916b86569e52094a674566879
siiiiilvio <s.ciarro@gmail.com>
2021-06-28 12:59:40 -0500

    Variable interpolation for window.location.pathname

--- examples/with-google-analytics/pages/_document.js
+++ examples/with-google-analytics/pages/_document.js
@@ -20,7 +20,7 @@ export default class MyDocument extends Document {
             gtag('js', new Date());
 
             gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
+              page_path: ${window.location.pathname},
             });
           `,
             }}
